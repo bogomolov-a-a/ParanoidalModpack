3dca8604331c865c77850df1965bf9ce87a47bf2
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-09-05 21:02:38 +0300

    возвращены типы помп, потому что ломается событие билда

--- mods/zzzparanoidal/control.lua
+++ mods/zzzparanoidal/control.lua
@@ -1,13 +1,21 @@
 require("prototypes.mod_compatibility.heroturrets_script") --скрипт разжалования турелей
 require("__PMRPGsystem__/prototypes/test")
 
+-- код для работы новых насосов
+local offshore_pump_types = { "offshore-mk0-pump", "offshore-pump", "offshore-mk2-pump", "offshore-mk3-pump",
+    "offshore-mk4-pump", "seafloor-pump", "seafloor-pump-2", "seafloor-pump-3" }
 --###############################################################################################
 -- from some corpse marker
 script.on_event(defines.events.on_pre_player_died, function(event)
     local player = game.players[event.player_index]
     player.force.add_chart_tag(player.surface,
-        { position = player.position, text = 'Corpse: ' ..
-        player.name .. '; Time: ' .. math.floor(game.tick / 60 / 60 / 60) .. ':' .. (math.floor(game.tick / 60 / 60) % 60), icon = { type = "virtual", name = "signal-info" } })
+        {
+            position = player.position,
+            text = 'Corpse: ' ..
+                player.name ..
+                '; Time: ' .. math.floor(game.tick / 60 / 60 / 60) .. ':' .. (math.floor(game.tick / 60 / 60) % 60),
+            icon = { type = "virtual", name = "signal-info" }
+        })
 end)
 
 if (settings.global["paranoidal-disable-vanilla-evolution"] or {}).value then
