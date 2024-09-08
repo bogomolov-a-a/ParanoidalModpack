8821b87056de36d1a71b6ada22f1d0bb0e4c992f
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-09-08 17:08:19 +0300

    Добавлена стадия скриптов

--- mods/automated-fabrique-protocol/interfaces/constants.lua
+++ mods/automated-fabrique-protocol/interfaces/constants.lua
@@ -1,5 +1,8 @@
 MOD_NAME = "automated-train-transport-protocol"
-ATTP_1_TECHNOLOGY_NAME = "automated-train-transport-protocol-t1"
-ATTP_2_TECHNOLOGY_NAME = "automated-train-transport-protocol-t2"
-ATTP_3_TECHNOLOGY_NAME = "automated-train-transport-protocol-t3"
-ATTP_4_TECHNOLOGY_NAME = "automated-train-transport-protocol-t4"
+ATTP_1_TECHNOLOGY_NAME = "basic-automated-train-transport-protocol"
+ATTP_2_TECHNOLOGY_NAME = "enhanced-automated-train-transport-protocol"
+ATTP_3_TECHNOLOGY_NAME = "advanced-automated-train-transport-protocol"
+ATTP_4_TECHNOLOGY_NAME = "full-automated-train-transport-protocol"
+STATION_BLOCK_HEAD = 1
+STATION_BLOCK_PAYLOAD = 2
+STATION_BLOCK_TRAIL = 3
