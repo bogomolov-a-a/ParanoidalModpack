5da9807bb7f8cd0b1c6efa8d7a80e3a087d3ec1c
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-09-22 19:06:42 +0300

    CI MUST BE IN SHELL!

--- build.sh
+++ build.sh
@@ -1,67 +1,26 @@
-#!/usr/bin/env bash
-
-bash --version 2>&1 | head -n 1
-
-set -eo pipefail
-SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-
-###########################################################################
-# CONFIGURATION
-###########################################################################
-
-BUILD_PROJECT_FILE="$SCRIPT_DIR/.nuke/_build.csproj"
-TEMP_DIRECTORY="$SCRIPT_DIR//.nuke/temp"
-
-DOTNET_GLOBAL_FILE="$SCRIPT_DIR//global.json"
-DOTNET_INSTALL_URL="https://dot.net/v1/dotnet-install.sh"
-DOTNET_CHANNEL="8.0"
-
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
-export DOTNET_NOLOGO=1
-
-###########################################################################
-# EXECUTION
-###########################################################################
-
-function FirstJsonValue {
-    perl -nle 'print $1 if m{"'"$1"'": "([^"]+)",?}' <<< "${@:2}"
-}
-
-# If dotnet CLI is installed globally and it matches requested version, use for execution
-if [ -x "$(command -v dotnet)" ] && dotnet --version &>/dev/null; then
-    export DOTNET_EXE="$(command -v dotnet)"
-else
-    # Download install script
-    DOTNET_INSTALL_FILE="$TEMP_DIRECTORY/dotnet-install.sh"
-    mkdir -p "$TEMP_DIRECTORY"
-    curl -Lsfo "$DOTNET_INSTALL_FILE" "$DOTNET_INSTALL_URL"
-    chmod +x "$DOTNET_INSTALL_FILE"
-
-    # If global.json exists, load expected version
-    if [[ -f "$DOTNET_GLOBAL_FILE" ]]; then
-        DOTNET_VERSION=$(FirstJsonValue "version" "$(cat "$DOTNET_GLOBAL_FILE")")
-        if [[ "$DOTNET_VERSION" == ""  ]]; then
-            unset DOTNET_VERSION
-        fi
-    fi
-
-    # Install by channel or version
-    DOTNET_DIRECTORY="$TEMP_DIRECTORY/dotnet-unix"
-    if [[ -z ${DOTNET_VERSION+x} ]]; then
-        "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --channel "$DOTNET_CHANNEL" --no-path
-    else
-        "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "$DOTNET_VERSION" --no-path
-    fi
-    export DOTNET_EXE="$DOTNET_DIRECTORY/dotnet"
-    export PATH="$DOTNET_DIRECTORY:$PATH"
-fi
-
-echo "Microsoft (R) .NET SDK version $("$DOTNET_EXE" --version)"
-
-if [[ ! -z ${NUKE_ENTERPRISE_TOKEN+x} && "$NUKE_ENTERPRISE_TOKEN" != "" ]]; then
-    "$DOTNET_EXE" nuget remove source "nuke-enterprise" &>/dev/null || true
-    "$DOTNET_EXE" nuget add source "https://f.feedz.io/nuke/enterprise/nuget" --name "nuke-enterprise" --username "PAT" --password "$NUKE_ENTERPRISE_TOKEN" --store-password-in-clear-text &>/dev/null || true
-fi
-
-"$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary --verbosity quiet
-"$DOTNET_EXE" run --project "$BUILD_PROJECT_FILE" --no-build -- "$@"
+export paranoidal_mods_path="$(cat config.json |jq -r '.modsPath')/$(cat config.json |jq -r '.mod.name')"
+export base_mod_version_data=$(cat "$paranoidal_mods_path/info.json" | jq -r ".dependencies "  |jq ".[0] " |xargs)
+if [[ $base_mod_version_data =~  ^(base >= )([0-9]{0,}\.[0-9]{0,}\.[0-9]{0,})$ ]]; then base_mod_version=${BASH_REMATCH[2]}; else exit 1; fi
+echo "Factorio version is $base_mod_version"
+wget  "$(cat config.json |jq -r '.serverDownloadLink')/$base_mod_version/$(cat config.json |jq -r '.version')/linux64" -O factorio.tar.xz && tar xf factorio.tar.xz
+./factorio/bin/x64/factorio  --version
+runner_hostname=$(hostname -f)
+./factorio/bin/x64/factorio --mod-directory $(cat config.json |jq -r '.modsPath') --start-server-load-scenario base/freeplay --bind $runner_hostname:34197 &
+factorio_server_pid=$!
+for((i=0; ;++i)); do 
+	sleep 1;
+	while   ps | grep " $factorio_server_pid "     
+	do   echo $factorio_server_pid is still in the ps output. Must still be running.
+		sleep 3
+	done
+	$my_statusopened_port=$(netstat -u -n -a);
+	if [[ $opened_port == *":34197"* ]]; then
+		echo "factorio server started at $opened_port. Mods validation on start with contol.lua completed! New game has been created!"; 
+		exit 0;  
+	fi
+	echo Oh, it looks like the process is done.
+	wait $factorio_server_pid
+	factorio_server_exit_code=$?
+	echo "factorio_server_exit_code $factorio_server_exit_code"	
+	exit $factorio_server_exit_code
+done;
