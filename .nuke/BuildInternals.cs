0d7f299ae4b8387bc818b69f3e0dfa727f701de9
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2024-09-22 19:06:42 +0300

    CI MUST BE IN SHELL!

--- .nuke/BuildInternals.cs
+++
@@ -1,9 +0,0 @@
-#nullable enable
-using System.Text.Json;
-using FactorioParanoidal.Models.PropertyTrees;
-
-partial class Build
-{
-    public JsonSerializerOptions SerializerOptions { get; }
-        = new() { WriteIndented = true, Converters = { new FactorioPropertyTreeJsonConverter() } };
-}
