cb73578ca6e7d0f2de9f3cbddcbb306261eb2784
Богомолов Артем <artem.bogomolov@iac.spb.ru>
2024-10-13 16:16:03 +0300

    Исправление зависимостей технологий military добавлена к cargo-planes

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/link-basic-technologies-to-regular-tree.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/link-basic-technologies-to-regular-tree.lua
@@ -314,6 +314,7 @@ local function add_prerequisites_to_technologies_in_regular_tree(mode)
     tech_util.add_prerequisites_to_technology("memory-unit",{"warehouse-research"},mode)
     tech_util.add_prerequisites_to_technology("vehicle-roboport-equipment",{"concrete"},mode)
     tech_util.add_prerequisites_to_technology("armor-absorb-20",{"utility-science-pack"},mode)
+    tech_util.add_prerequisites_to_technology("cargo-planes",{"military"},mode)
 end
 
 local function remove_prerequisites_from_technologies_in_regular_tree(mode)
