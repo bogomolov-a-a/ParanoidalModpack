f463788c6c7949fd8e9b64198752f9d2ad353317
Богомолов Артем <artem.bogomolov@iac.spb.ru>
2024-10-14 18:26:40 +0300

    Исправление зависимостей технологий concrete добавлена к angels-advanced-chemistry-2

--- mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/link-basic-technologies-to-regular-tree.lua
+++ mods/hardcore-mode-for-playing-for-zzzparanoidal/prototypes/technologies/normal-techonology-tree-linking/link-basic-technologies-to-regular-tree.lua
@@ -327,6 +327,7 @@ local function add_prerequisites_to_technologies_in_regular_tree(mode)
     tech_util.add_prerequisites_to_technology("angels-sulfur-processing-2",{"electronics-machine-2"},mode)
     tech_util.add_prerequisites_to_technology("angels-nitrogen-processing-2",{"angels-bronze-smelting-1"},mode)    
     tech_util.add_prerequisites_to_technology("angels-advanced-chemistry-1",{"angels-bronze-smelting-1"},mode)
+    tech_util.add_prerequisites_to_technology("angels-advanced-chemistry-2",{"concrete"},mode)
 end
 
 local function remove_prerequisites_from_technologies_in_regular_tree(mode)
