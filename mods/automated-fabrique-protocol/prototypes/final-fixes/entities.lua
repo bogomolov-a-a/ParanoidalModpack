676ef4d5d8402ecd332197a1bf47d5e0f0a05258
Богомолов Артем <artem.bogomolov@iac.spb.ru>
2024-10-24 13:42:58 +0300

    Вагоны полезной нагрузки расцеплятся не будут(любого состава), вагоны с локомотивами расцепляются и обслуживаются отдельно.

--- mods/automated-fabrique-protocol/prototypes/final-fixes/entities.lua
+++ mods/automated-fabrique-protocol/prototypes/final-fixes/entities.lua
@@ -4,17 +4,17 @@ _table.each(data.raw["train-stop"],
         train_stop_prototype.selection_box = { { -0.5, 0.5 }, { 0.5, 1.5 } }
     end)
 _table.each(data.raw["locomotive"], function(rolling_stock)
-    rolling_stock.connection_distance = 4.05
-    rolling_stock.joint_distance = 3.1
-    rolling_stock.collision_box = { { -1, -1.75 }, { 1, 1.75 } }
+    rolling_stock.connection_distance = 3.9
+    rolling_stock.joint_distance = 3
+    rolling_stock.collision_box = { { -1, -1.7 }, { 1, 1.7 } }
     rolling_stock.drawing_box = { { -1, -2.1 }, { 1, 2.1 } }
     rolling_stock.vertical_selection_shift = -0.5
     rolling_stock.selection_box = { { -1, -2.5 }, { 1, 2.5 } }
 end)
 local function handle_cargo_wagon_rolling_stock(rolling_stock)
-    rolling_stock.connection_distance = 4.05
-    rolling_stock.joint_distance = 2.8
-    rolling_stock.collision_box = { { -1, -1.6 }, { 1, 1.6 } }
+    rolling_stock.connection_distance = 4
+    rolling_stock.joint_distance = 2.92
+    rolling_stock.collision_box = { { -1, -1.66 }, { 1, 1.66 } }
     rolling_stock.drawing_box = { { -1, -2.1 }, { 1, 2.1 } }
     rolling_stock.vertical_selection_shift = -0.5
     rolling_stock.selection_box = { { -1, -2.5 }, { 1, 2.5 } }
